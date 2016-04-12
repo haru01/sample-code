7f156496281e7b32b159e98385aa70d02aac59ec
Matt Karl <matt.karl@jibo.com>
2016-04-12 11:32:39 -0400

    Removed setInterval and replaced with new timer module

--- src/main.js
+++ src/main.js
@@ -3,27 +3,23 @@
 let jibo = require ('jibo');
 let Status = jibo.bt.Status;
 
-function start() {
-    let root = jibo.bt.create('../behaviors/08-idle');
-    root.start();
-    let intervalId = setInterval(function() {
-        if (root.status !== Status.IN_PROGRESS) {
-            clearInterval(intervalId);
-            console.log('Behavior tree finished with status ' + root.status);
-        } else {
-            root.update();
-        }
-    }, 33);
-}
-
-jibo.init(function() {
-    //We need to require any custom behaviors so that they can register themselves
-    //with the behavior factory.
+jibo.init('eye', function() {
+    // We need to require any custom behaviors so that they can 
+    // register themselves with the behavior factory.
     require('./behaviors/center-robot');
     require('./behaviors/succeed-on-touch');
     require('./behaviors/fail-on-touch');
-    //Setup code for displaying Jibo's eye.
-    let eyeElement = document.getElementById('eye');
-    jibo.visualize.createRobotRenderer(eyeElement, jibo.visualize.DisplayType.EYE);
-    start();
+
+    // Load and create the behavior tree
+    let root = jibo.bt.create('../behaviors/08-idle');
+    root.start();
+
+    // Listen for frame update for change in status
+    jibo.timer.on('update', function(elapsed){
+        if (root.status !== Status.IN_PROGRESS) {
+            console.log('Behavior tree finished with status: %s', root.status);
+        } else {
+            root.update();
+        }
+    });
 });
