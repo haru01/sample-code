3599367fc0fbabf353569d45c8b195423ac9982f
Matt Karl <matt.karl@jibo.com>
2016-04-19 14:49:36 -0400

    Error catching with init and eye -> face

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@
 let jibo = require ('jibo');
 let Status = jibo.bt.Status;
 
-jibo.init('eye', function() {
+jibo.init('face', function(err) {
+    if (err) {
+        return console.error(err);
+    }
     // We need to require any custom behaviors so that they can 
     // register themselves with the behavior factory.
     require('./behaviors/center-robot');
